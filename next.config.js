b7e31d53223aae54753f80dea35b61ae13e0b2a6
Aryan <paryan@digitalocean.com>
2025-07-16 17:12:20 +0100

    Configure

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/my-account/**",
+        pathname: "/**",
         search: "",
       },
     ],
