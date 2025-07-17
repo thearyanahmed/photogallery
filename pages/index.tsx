eb440decfd1c2fa53fa3f7b9ae958f79291eb4c4
Aryan <paryan@digitalocean.com>
2025-07-17 13:40:52 +0100

    Increase images

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,7 +70,7 @@ const StaticPage: NextPage<{ images: ImageProps[] }> = ({ images }) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const images: ImageProps[] = getImages(20, null);
+  const images: ImageProps[] = getImages(50, null);
 
   const blurImagePromises = images.map((image) => getBase64ImageUrl(image));
   const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
